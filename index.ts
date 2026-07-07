9fd6063a6cb0d2bc350b7731aad581ba253fed10
Developer <developer@example.com>
2026-07-07 02:24:38 +0000

    chore: drop gtps.png, add favicon link to inline dashboard

--- index.ts
+++ index.ts
@@ -9,6 +9,8 @@ const DASHBOARD_HTML = `<!DOCTYPE html>
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Growtopia Player Support</title>
+  <link rel="icon" type="image/svg+xml" href="/animated-logo.svg">
+  <link rel="shortcut icon" href="/animated-logo.svg" type="image/svg+xml">
   <style>
     * { box-sizing: border-box; margin: 0; padding: 0; }
     body {
